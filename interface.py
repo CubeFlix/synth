52cd0cec90137374e8cd11c2017b2a30ee872468
cubeflix <kevin.signal@gmail.com>
2023-08-13 20:38:08 -0500

    debug info

--- interface.py
+++ interface.py
@@ -1,11 +1,12 @@
 import tkinter
 import tkinter.messagebox
+from tkinter import ttk
 import rtmidi
 import pyaudio
 import numpy as np
 import threading
 from dataclasses import dataclass
-import math
+import os
 
 SAMPLE_RATE = 44100
 TIMEOUT = 30
@@ -23,11 +24,14 @@ def calculate_pitch_et(pitch, et):
 class SynthInterface:
     def __init__(self, title="Kevin's Synth"):
         self.title = title
+        self.running = False
+
+        # Settings.
         self.port = None
         self.volume = 100
         self.hertz = 0
         self.et = 12
-        self.running = False
+        self.should_attack_decay_smoothing = True
 
     def get_midi_inputs(self):
         return [self.midi.getPortName(i) for i in range(self.midi.getPortCount())]
@@ -37,58 +41,83 @@ class SynthInterface:
         self.root = tkinter.Tk()
         self.root.title(self.title)
         self.root.resizable(False, False)
+        def on_close_window():
+            self.stop_synth()
+            self.root.destroy()
+            os._exit(0)
+        self.root.protocol("WM_DELETE_WINDOW", on_close_window)
 
         self.frame_left = tkinter.Frame(self.root)
+        self.frame_center = tkinter.Frame(self.root)
         self.frame_right = tkinter.Frame(self.root)
+        self.frame_debug = tkinter.Frame(self.root)
 
         self.midi_inputs_listbox_label = tkinter.Label(self.frame_left, text="Select a MIDI input:")
         self.midi_inputs_listbox_label.pack()
         self.midi_inputs_listbox = tkinter.Listbox(self.frame_left)
         self.midi_inputs_listbox.pack()
-        self.midi_inputs_select_button = tkinter.Button(self.frame_left, text="Select", command=self.select_midi_input)
+        self.midi_inputs_select_button = ttk.Button(self.frame_left, text="Select", command=self.select_midi_input)
         self.midi_inputs_select_button.pack()
-        self.midi_inputs_update_button = tkinter.Button(self.frame_left, text="Update", command=self.update_midi_inputs)
+        self.midi_inputs_update_button = ttk.Button(self.frame_left, text="Update", command=self.update_midi_inputs)
         self.midi_inputs_update_button.pack()
         self.midi_status_label_var = tkinter.StringVar(self.root, "No MIDI input selected.")
         self.midi_status_label = tkinter.Label(self.frame_left, textvariable=self.midi_status_label_var)
         self.midi_status_label.pack()
 
-        self.volume_label_var = tkinter.StringVar(self.root, "Volume: 100%")
-        self.volume_label = tkinter.Label(self.frame_right, textvariable=self.volume_label_var)
-        self.volume_label.pack()
-        self.volume_slider = tkinter.Scale(self.frame_right, from_=0, to_=100, orient=tkinter.HORIZONTAL, command=self.update_volume)
-        self.volume_slider.pack()
-        self.volume_slider.set(100)
         self.tuning_label_var = tkinter.StringVar(self.root, "Tuning system: 12-et")
-        self.tuning_label = tkinter.Label(self.frame_right, textvariable=self.tuning_label_var)
+        self.tuning_label = tkinter.Label(self.frame_center, textvariable=self.tuning_label_var)
         self.tuning_label.pack()
-        self.tuning_slider = tkinter.Scale(self.frame_right, from_=1, to_=64, orient=tkinter.HORIZONTAL, command=self.update_et)
+        self.tuning_slider = tkinter.Scale(self.frame_center, from_=1, to_=64, orient=tkinter.HORIZONTAL, command=self.update_et)
         self.tuning_slider.pack()
         self.tuning_slider.set(12)
         self.hertz_label_var = tkinter.StringVar(self.root, "Re-tune: 0hz")
-        self.hertz_label = tkinter.Label(self.frame_right, textvariable=self.hertz_label_var)
+        self.hertz_label = tkinter.Label(self.frame_center, textvariable=self.hertz_label_var)
         self.hertz_label.pack()
-        self.hertz_slider = tkinter.Scale(self.frame_right, from_=-100, to_=100, orient=tkinter.HORIZONTAL, command=self.update_hertz)
+        self.hertz_slider = tkinter.Scale(self.frame_center, from_=-100, to_=100, orient=tkinter.HORIZONTAL, command=self.update_hertz)
         self.hertz_slider.pack()
         self.hertz_slider.set(0)
-        self.reset_settings_button = tkinter.Button(self.frame_right, text="Reset Settings", command=self.reset_settings)
+        self.volume_label_var = tkinter.StringVar(self.root, "Volume: 100%")
+        self.volume_label = tkinter.Label(self.frame_center, textvariable=self.volume_label_var)
+        self.volume_label.pack()
+        self.volume_slider = tkinter.Scale(self.frame_center, from_=0, to_=100, orient=tkinter.HORIZONTAL, command=self.update_volume)
+        self.volume_slider.pack()
+        self.volume_slider.set(100)
+        
+        self.should_attack_decay_smoothing_checkbox = ttk.Checkbutton(self.frame_right, text="Apply attack/decay smoothing", onvalue=True, offvalue=False, command=self.update_should_attack_decay_smoothing)
+        self.should_attack_decay_smoothing_checkbox.pack()
+        self.should_attack_decay_smoothing_checkbox.state(['selected'])
+        self.reset_settings_button = ttk.Button(self.frame_right, text="Reset Settings", command=self.reset_settings)
         self.reset_settings_button.pack()
-        self.start_synth_button = tkinter.Button(self.frame_right, text="Start Synth", command=self.start_synth)
+        self.start_synth_button = ttk.Button(self.frame_right, text="Start Synth", command=self.start_synth)
         self.start_synth_button.pack()
-        self.stop_synth_button = tkinter.Button(self.frame_right, text="Stop Synth", command=self.stop_synth)
+        self.stop_synth_button = ttk.Button(self.frame_right, text="Stop Synth", command=self.stop_synth)
         self.stop_synth_button.pack()
         self.synth_status_label_var = tkinter.StringVar(self.root, "Synth stopped.")
         self.synth_status_label = tkinter.Label(self.frame_right, textvariable=self.synth_status_label_var)
         self.synth_status_label.pack()
+        
+        self.synth_debug_label_var_1 = tkinter.StringVar(self.root, f"Sample rate: {SAMPLE_RATE}")
+        self.synth_debug_label_1 = tkinter.Label(self.frame_debug, textvariable=self.synth_debug_label_var_1)
+        self.synth_debug_label_1.pack()
+        self.synth_debug_label_var_2 = tkinter.StringVar(self.root, f"Timeout duration: {TIMEOUT}")
+        self.synth_debug_label_2 = tkinter.Label(self.frame_debug, textvariable=self.synth_debug_label_var_2)
+        self.synth_debug_label_2.pack()
+        self.synth_debug_label_var_3 = tkinter.StringVar(self.root, "Num notes: [synth inactive]")
+        self.synth_debug_label_3 = tkinter.Label(self.frame_debug, textvariable=self.synth_debug_label_var_3)
+        self.synth_debug_label_3.pack()
+        self.synth_debug_label_var_4 = tkinter.StringVar(self.root, "Num frames: [synth inactive]")
+        self.synth_debug_label_4 = tkinter.Label(self.frame_debug, textvariable=self.synth_debug_label_var_4)
+        self.synth_debug_label_4.pack()
 
         self.frame_left.pack(expand=True, fill=tkinter.BOTH, side=tkinter.LEFT)
+        self.frame_center.pack(expand=True, fill=tkinter.BOTH, side=tkinter.LEFT)
         self.frame_right.pack(expand=True, fill=tkinter.BOTH, side=tkinter.LEFT)
+        self.frame_debug.pack(expand=True, fill=tkinter.BOTH, side=tkinter.LEFT)
 
         self.update_midi_inputs()
     
     def run(self):
         self.root.mainloop()
-        self.stop_synth()
 
     def update_volume(self, _):
         self.volume = self.volume_slider.get()
@@ -101,6 +130,9 @@ class SynthInterface:
     def update_hertz(self, _):
         self.hertz = self.hertz_slider.get()
         self.hertz_label_var.set(f"Re-tune: {self.hertz}hz")
+
+    def update_should_attack_decay_smoothing(self):
+        self.should_attack_decay_smoothing = 'selected' in self.should_attack_decay_smoothing_checkbox.state()
     
     def reset_settings(self):
         self.volume_slider.set(100)
@@ -109,6 +141,8 @@ class SynthInterface:
         self.update_et(None)
         self.hertz_slider.set(0)
         self.update_hertz(None)
+        self.should_attack_decay_smoothing_checkbox.state(['selected'])
+        self.update_should_attack_decay_smoothing()
 
     def select_midi_input(self):
         selected = self.midi_inputs_listbox.curselection()
@@ -151,8 +185,8 @@ class SynthInterface:
         self.buffer = np.zeros(int(SAMPLE_RATE * TIMEOUT / 1000))
         self.calculate_pitch = calculate_pitch_et
         self.attack_smoothing = self.base / len(self.base)
-        self.decay_smoothing = (TIMEOUT / 1000 - self.base / (SAMPLE_RATE)) / (TIMEOUT / 1000)
-        
+        self.decay_smoothing = (TIMEOUT / 1000 - self.base / SAMPLE_RATE) / (TIMEOUT / 1000)
+
         self.running = True
         
         # Start the sound generation loop.
@@ -164,8 +198,11 @@ class SynthInterface:
         threading.Thread(target=lambda: self.midi_input_thread()).start()
 
         self.synth_status_label_var.set("Synth started.")
+        self.synth_debug_label_var_3.set(f"Num notes: {len(self.current_notes)}")
+        self.synth_debug_label_var_4.set(f"Num frames: {self.num_frames_count}")
 
     def sound_play_thread(self):
+        self.sound_thread_running = True
         while self.running:
             # Zero the buffer and calculate the sounds.
             self.buffer -= self.buffer
@@ -174,22 +211,27 @@ class SynthInterface:
                 sound = note.velocity / 400 * (np.sin(2 * np.pi * (self.base + self.num_frames_count * int(SAMPLE_RATE * TIMEOUT / 1000)) * pitch / SAMPLE_RATE)) * (self.volume / 100)
                 if note.is_new:
                     # Apply attack smoothing.
-                    sound *= self.attack_smoothing
+                    if self.should_attack_decay_smoothing:
+                        sound *= self.attack_smoothing
                     note.is_new = False
                 if note.should_remove:
                     # Apply decay smoothing and delete the note.
-                    # # We calculate the greatest number of cycles we can have so we end at a 0.
-                    # max_samples_to_zero = math.floor(pitch * TIMEOUT/1000) / pitch * SAMPLE_RATE
-                    # offset = (self.num_frames_count * int(SAMPLE_RATE * TIMEOUT / 1000)) % (int(1/pitch * SAMPLE_RATE))
-                    # sound[int(max_samples_to_zero - offset):] = 0.0 
-                    # print(sound[-100:])
-                    sound *= self.decay_smoothing
+                    if self.should_attack_decay_smoothing:
+                        sound *= self.decay_smoothing
                     del self.current_notes[self.find_note_by_number(note.pitch)]
                 self.buffer += sound
             self.num_frames_count += 1
+            if len(self.current_notes) == 0:
+                self.num_frames_count = 0
 
             # Write the sound to the output buffer.
             self.stream.write(self.buffer.astype(np.float32).tobytes())
+            
+            self.synth_debug_label_var_3.set(f"Num notes: {len(self.current_notes)}")
+            self.synth_debug_label_var_4.set(f"Num frames: {self.num_frames_count}")
+        
+        self.synth_debug_label_var_3.set("Num notes: [synth inactive]")
+        self.synth_debug_label_var_4.set("Num frames: [synth inactive]")
 
     def find_note_by_number(self, num):
         for i, note in enumerate(self.current_notes):
@@ -208,8 +250,6 @@ class SynthInterface:
                 self.current_notes.append(note)
             elif message.isNoteOff():
                 self.current_notes[self.find_note_by_number(message.getNoteNumber())].should_remove = True
-            if len(self.current_notes) == 0:
-                self.num_frames_count = 0
 
     def stop_synth(self):
         if self.running == False:
